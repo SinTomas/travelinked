86cc16271a55a38cd0e01ff4b0aa140ec11f6922
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-05-15 18:01:31 +0100

    changes

--- routes/countries.routes.js
+++ routes/countries.routes.js
@@ -1,17 +1,27 @@
 let router = require("express").Router();
 let Country = require("../models/country.Model");
-const axios = require("axios");
+let axios = require("axios");
 
 //Get all Countries
 
 router.get("/countries", async (req, res) => {
-  const apiCall = await axios.get("https://restcountries.com/v3.1/all");
+  let apiCall = await axios.get("https://restcountries.com/v3.1/all");
   console.log("path to currency", apiCall.data[0].currencies);
-  res.render("countries/all-countries", { countries: apiCall.data, currency: apiCall.data[0].currencies });
+  res.render("countries/all-countries", {countries: apiCall.data});
 });
 
 router.get("/countries/create", async (req, res) => {
   res.render("countries/add-country");
 });
+
+router.get("/about", async (req, res) => {
+  res.render("about");
+});
+
+router.get("/europe", async (req, res) => {
+  let apiCall = await axios.get("https://restcountries.com/v3.1/all");
+console.log(apiCall.data)
+  res.render("countries/europe", {countries: apiCall.data});
+})
 
 module.exports = router;
