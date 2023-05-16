ce3e0b39ab67840660132a81ec9169200c56b016
Francisco Crespo <fmfcrespo@gmail.com>
2023-05-16 10:47:29 +0100

    filtered europe

--- routes/countries.routes.js
+++ routes/countries.routes.js
@@ -2,12 +2,37 @@ let router = require("express").Router();
 let Country = require("../models/country.Model");
 const axios = require("axios");
 
-//Get all Countries
+//Get all Countries in the world
 
 router.get("/countries", async (req, res) => {
   const apiCall = await axios.get("https://restcountries.com/v3.1/all");
-  console.log("path to currency", apiCall.data[0].currencies);
-  res.render("countries/all-countries", { countries: apiCall.data, currency: apiCall.data[0].currencies });
+  await Country.create({
+    flag: flags.png,
+    name: name.common,
+    continent: continents,
+    population,
+    capital,
+    currency: currencies[0].name,
+    currencySymbol: currencies[0].symbol,
+  });
+  res.render("countries/all-countries", { countries: apiCall.data });
+});
+
+//Get all Countries in Europe
+router.get("/countries/europe", async (req, res) => {
+  const apiCall = await axios.get("https://restcountries.com/v3.1/all");
+  let allCountries = apiCall.data;
+  /* console.log(allCountries[3].continents[0]); */
+  let europe = [];
+
+  for (let i = 0; i < allCountries.length; i++) {
+    if (allCountries[i].continents[0] === "Europe") {
+      europe.push(allCountries[i]);
+    }
+  }
+  console.log(europe);
+
+  res.render("countries/europe", { europe });
 });
 
 router.get("/countries/create", async (req, res) => {
