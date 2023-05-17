80eba6fd3bc172315d6aec1332325f84b895c9b2
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-05-17 17:56:10 +0100

    profile fixed

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ let mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-let MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/travelinked";
+let MONGO_URI = `mongodb+srv://SinTomas:${process.env.MONGODB_PASSWORD}@travelink.l3ltdjs.mongodb.net/?retryWrites=true&w=majority` || "mongodb://127.0.0.1/travelinked";
 
 mongoose
   .connect(MONGO_URI)
