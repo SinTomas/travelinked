8aad8a90016a706a56855570549c81d431c46de7
Francisco Crespo <fmfcrespo@gmail.com>
2023-05-17 12:39:22 +0100

    edit visited countries route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,6 +4,8 @@ let isLoggedIn = require('../middleware/isLoggedIn');
 let Country = require("../models/Countries.model");
 let axios = require("axios");
 
+
+
 //Get User from data base by ID Route
 router.get("/profile", isLoggedIn, async (req, res) => {
 
@@ -17,6 +19,8 @@ let user = req.session.currentUser._id;
     console.log(error);
   }
 });
+
+
 
 //Edit Profile Route
 router.get("/edit-profile", isLoggedIn, async (req, res) => {
@@ -44,12 +48,16 @@ router.post("/edit-profile", isLoggedIn, async (req, res) => {
       catch (error) {console.log(error);}
   });
 
+
+
 //About Route
 router.get("/about", async (req, res) => {
   res.render("about");
 });
 
-//Add a Visited Country to the Profile Page
+
+
+//Add a Visited Country to the Profile Page Route
 router.get("/add-visited", isLoggedIn, async (req, res) => {
 
   let user = req.session.currentUser._id;
@@ -62,6 +70,8 @@ router.get("/add-visited", isLoggedIn, async (req, res) => {
 
     catch (error) {console.log(error);}
 });
+
+
 
 router.post("/add-visited", isLoggedIn, async (req, res) => {
 let user = req.session.currentUser._id;
@@ -76,17 +86,47 @@ catch (error) {console.log(error);}
 });
 
 
+
+//Delete Visited  Country Route
 router.post("/remove-visited/:id", isLoggedIn, async (req, res) => {
   const {id} = req.params
 let user = req.session.currentUser._id;
-
-
 try {
   await User.findByIdAndUpdate(user, {$pull: {visitedCountries: id}});
   res.redirect("/profile");} 
 
 catch (error) {console.log(error);}
 });
+
+
+
+//Edit Visited Countries Route
+router.get("/edit-visited", isLoggedIn, async (req, res) => {
+
+  let user = req.session.currentUser._id;
+  
+    try {
+      let foundUser = await User.findById(user);
+      let countries = await Country.find();
+      res.render("users/edit-profile", {foundUser, countries});
+      console.log(countries);
+    }
+    catch (error) {console.log(error);
+    }
+});
+  
+router.post("/edit-visited", isLoggedIn, async (req, res) => {
+  let user = req.session.currentUser._id;
+  
+  let { visitedCountries} = req.body;
+  
+  try {
+    await User.findByIdAndUpdate(user, {$push: {visitedCountries: visitedCountries}});
+    res.redirect("/profile");} 
+  
+  catch (error) {console.log(error);}
+  });
+
 
 
 module.exports=router;
