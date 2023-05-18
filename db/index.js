c00c69d74039151410d8eef65ab68db9a9432779
Tomas Maria <tomaslbpmaria@hotmail.com>
2023-05-18 09:18:05 +0100

    mongoose strictquery

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ let mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 let MONGO_URI = `mongodb+srv://SinTomas:${process.env.MONGODB_PASSWORD}@travelink.l3ltdjs.mongodb.net/?retryWrites=true&w=majority`;
-
+mongoose.set('strictQuery', false);
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
